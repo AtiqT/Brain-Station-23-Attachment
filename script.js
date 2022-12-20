6ab1f7d41edf4699690eef4496aef9618a485449
AtiqT <120324190+AtiqT@users.noreply.github.com>
2022-12-20 11:38:27 +0600

    Add files via upload
    fundamentals 2

--- script.js
+++ script.js
@@ -1,82 +1,104 @@
-//JS Fundamental part 2:
-//003:
-function fun() {
-    console.log("I am Atiq");
-}
-
-fun();
-
-function JuicePro(apples, oranges) {
-    console.log(apples, oranges);
-    const juice = 'Juice with ' + apples + ' apples and ' + oranges + ' oranges.';
-    return juice;
-}
-const AppleJuice = JuicePro(5,0);
-console.log(AppleJuice);
-
-// Arrow functions
-
-const Age = birthYeah => 2037 - birthYeah;
-const a = Age(1991);
-console.log(a);
-
-const yearsRetirement = (birthYeah, firstName) => {
-  const age = 2037 - birthYeah;
-  const retirement = 65 - age;
-  // return retirement;
-  return '${firstName} retires in ${retirement} years';
-}
-console.log(yearsRetirement(1991, 'Jonas')); console.log(yearsRetirement(1980, 'Bob'));
-
-//coding ch#1:
-const Average = (a, b, c) => (a + b + c) / 3;
-console.log(Average(3, 4, 5));
-
-// Test 1
-let DolphinSc = Average(44, 23, 71);
-let KoalasSc = Average(65, 54, 49);
-console.log(DolphinSc, KoalasSc);
-
-const checkWinner = function (avgDolphins, avgKoalas) {
-  if (avgDolphins >= 2 * avgKoalas) {
-    console.log('Dolphins win 🏆 (${avgDolphins} vs. ${avgKoalas})');
-  } else if (avgKoalas >= 2 * avgDolphins) {
-    console.log('Koalas win 🏆 (${avgKoalas} vs. ${avgDolphins})');
-  } else {
-    console.log('No team wins...');
-  }
-}
-checkWinner(DolphinSc, KoalasSc);
-
-checkWinner(576, 111);
-
-// Test 2
-DolphinSc = Average(85, 54, 41);
-KoalasSc = Average(23, 34, 27);
-console.log(DolphinSc, KoalasSc);
-checkWinner(DolphinSc, KoalasSc);
-
-// Introduction to Arrays
-const friend1 = 'Michael';
-const friend2 = 'Steven';
-const friend3 = 'Peter';
-
+// Basic Array Operations (Methods)
 const friends = ['Michael', 'Steven', 'Peter'];
+
+const newL = friends.push('Jay');
+console.log(friends);
+console.log(newL);
+
+friends.unshift('John');
 console.log(friends);
 
-const y = new Array(1991, 1984, 2008, 2020);
-
-console.log(friends[0]);
-console.log(friends[2]);
-
-console.log(friends.length);
-console.log(friends[friends.length - 1]);
-
-friends[2] = 'Jay';
+friends.pop();
+const popped = friends.pop();
+console.log(popped);
 console.log(friends);
 
-const firstName = 'Jonas';
-const jonas = [firstName, 'Schmedtmann', 2037 - 1991, 'teacher', friends];
-console.log(jonas);
-console.log(jonas.length);
+friends.shift();
+console.log(friends);
+
+console.log(friends.indexOf('Steven'));
+console.log(friends.indexOf('Bob'));
+
+friends.push(23);
+console.log(friends.includes('Steven'));
+console.log(friends.includes('Bob'));
+console.log(friends.includes(23));
+
+if (friends.includes('Steven')) {
+  console.log('Steven');
+}
+
+// Coding Challenge #2:
+
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+console.log(bills, tips, totals);
+
+// Coding Challenge #3:
+
+const mark = {
+  fullName: 'Mark Miller',
+  mass: 78,
+  height: 1.69,
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  }
+};
+
+const john = {
+  fullName: 'John Smith',
+  mass: 92,
+  height: 1.95,
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  }
+};
+
+mark.calcBMI();
+john.calcBMI();
+
+console.log(mark.bmi, john.bmi);
+
+if (mark.bmi > john.bmi) {
+  console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s BMI (${john.bmi})`)
+} else if (john.bmi > mark.bmi) {
+  console.log(`${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s BMI (${mark.bmi})`)
+}
+
+
+//coding Ch4:
+const TipC = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+const bill = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tip = [];
+const total = [];
+
+for (let i = 0; i < bill.length; i++) {
+  const tip = calcTip(bill[i]);
+  tips.push(tip);
+  totals.push(tip + bill[i]);
+}
+console.log(bill, tip, total);
+
+const Average = function (arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum / arr.length;
+}
+console.log(Average([2, 3, 7]));
+console.log(Average(total));
+console.log(Average(tip));
+
+
 
